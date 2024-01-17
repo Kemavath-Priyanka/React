9b4632db92070640f03e9f78b9e4ee2502217438
Priyanka Bai Kemavath <priyankabai.kemavath@hcl.com>
2024-01-17 17:52:00 +0530

    Implemented Tic-Tac-Toe Game

---
+++ 02 Tic Tac Toe Game/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,36 @@
+export default function GameBoard({ onSelectSqaure, board }) {
+  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  //   function handleSelectSquare(rowIndex, colIndex) {
+  //     // Updating state in immutuable way - recommended way
+  //     setGameBoard((prevGameBoard) => {
+  //       const updatedBoard = [
+  //         ...prevGameBoard.map((innerArray) => [...innerArray])
+  //       ];
+  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //       return updatedBoard;
+  //     });
+  //     onSelectSqaure();
+  //   }
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSqaure(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
